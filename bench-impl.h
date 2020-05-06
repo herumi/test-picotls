c7f756f4ca93195669f2d7955a03e8c354fb2684
MITSUNARI Shigeo <herumi@nifty.com>
2020-05-07 08:32:09 +0900

    unify

---
+++ bench-impl.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <picotls/fusion.h>
+
+int main(int argc, char **argv)
+{
+    static const uint8_t key[16] = {}, iv[12] = {}, aad[13] = {}, text[16384] = {};
+    ptls_fusion_aesgcm_context_t ctx;
+    uint8_t encrypted[sizeof(text) + 16];
+    size_t textlen = 16384;
+
+#ifdef USE_SSCANF
+    if (sscanf(argv[1], "%zu", &textlen) != 1) {
+        fprintf(stderr, "failed to obtain text length from argument\n");
+        return 1;
+    }
+#endif
+
+    ptls_fusion_aesgcm_init(&ctx, key);
+
+    for (int i = 0; i < 1000000; ++i)
+        ptls_fusion_aesgcm_encrypt(&ctx, iv, aad, sizeof(aad), encrypted, text, textlen);
+
+
+    for (int i = 0; i < 16; ++i)
+        printf("%02x", encrypted[sizeof(text) + i]);
+    printf("\n");
+
+    return 0;
+}
